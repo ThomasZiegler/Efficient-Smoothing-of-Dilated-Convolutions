48fc9c30deff037ffc96ec3d96ecd44b4f47390c
Thomas Ziegler <zieglert@ethz.ch>
2018-11-27 18:20:20 +0100

    fix tab error

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+# allow print to be displayed in the cluster job logs
+from __future__ import print_function
+
 import argparse
 import os
 import tensorflow as tf
 from model import Model
-# allow print to be displayed in the cluster job logs
-from __future__ import print_function
-
 """
 This script defines hyperparameters.
 """
@@ -24,9 +24,7 @@ def configure():
 	flags.DEFINE_float('momentum', 0.9, 'momentum')
 	flags.DEFINE_string('encoder_name', 'deeplab', 'name of pre-trained model: res101, res50 or deeplab')
 	flags.DEFINE_string('pretrain_file', '../reference model/deeplab_resnet_init.ckpt', 'pre-trained model filename corresponding to encoder_name')
-	flags.DEFINE_string('dilated_type', 'smooth_GI', 'type of dilated conv:
-                     regular, decompose, smooth_GI, smooth_SSC or
-                     average_filter')
+	flags.DEFINE_string('dilated_type', 'smooth_GI', 'type of dilated conv: regular, decompose, smooth_GI, smooth_SSC or average_filter')
 	flags.DEFINE_string('data_list', './dataset/train.txt', 'training data list filename')
 
 	# validation
