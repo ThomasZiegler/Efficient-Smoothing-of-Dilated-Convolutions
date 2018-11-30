c7d3e70f50294fecd9c8b81d957f798534e2ef8f
Thomas Ziegler <zieglert@ethz.ch>
2018-11-30 17:43:52 +0100

    Use environmental constants

--- main.py
+++ main.py
@@ -13,54 +13,43 @@ This script defines hyperparameters.
 
 
 
-#def configure(start_step_ = 0, num_steps_ = 2000, pretrain_file_ =
-#              '../reference_model/deeplab_resnet_init.ckpt', valid_step_ = 2000):
 def configure():
     flags = tf.app.flags
 
     # training
-#    flags.DEFINE_integer('start_step', start_step_, 'start number of iterations')
-#    flags.DEFINE_integer('num_steps', num_steps_, 'maximum number of iterations')
     flags.DEFINE_integer('start_step', 0, 'start number of iterations')
     flags.DEFINE_integer('num_steps', 20, 'maximum number of iterations')
     flags.DEFINE_integer('max_steps', 20, 'maximum number of iterations')
     flags.DEFINE_integer('save_interval', 10000, 'number of iterations for saving and visualization')
     flags.DEFINE_integer('random_seed', 1234, 'random seed')
-    flags.DEFINE_float('weight_decay', 0.0005, 'weight decay rate')
+    flags.DEFINE_float('weight_decay', 0.0001, 'weight decay rate')
     flags.DEFINE_float('learning_rate', 0.001, 'learning rate')
     flags.DEFINE_float('power', 0.9, 'hyperparameter for poly learning rate')
     flags.DEFINE_float('momentum', 0.9, 'momentum')
     flags.DEFINE_string('encoder_name', 'deeplab', 'name of pre-trained model: res101, res50 or deeplab')
-#    flags.DEFINE_string('pretrain_file', pretrain_file_, 'pre-trained model filename corresponding to encoder_name')
     flags.DEFINE_string('pretrain_file', '../reference_model/deeplab_resnet_init.ckpt', 'pre-trained model filename corresponding to encoder_name')
-#    flags.DEFINE_string('pretrain_file', '../reference_model/resnet_v1_101.ckpt', 'pre-trained model filename corresponding to encoder_name')
-#    flags.DEFINE_string('pretrain_file', None, 'pre-trained model filename corresponding to encoder_name')
     flags.DEFINE_string('checkpoint_file', '../reference_model/deeplab_resnet_init.ckpt', 'checkpoint model filename corresponding to encoder_name')
     flags.DEFINE_string('dilated_type', 'regular', 'type of dilated conv: regular, decompose, smooth_GI, smooth_SSC or average_filter')
-    flags.DEFINE_string('data_list', './dataset/train.txt', 'training data list filename')
-
-    # train & validate
-    flags.DEFINE_integer('num_iterations', 2, 'number of test & validate iterations')
+    flags.DEFINE_string('data_list', os.environ['DATALIST'], 'training data list filename')
 
     # validation
-#    flags.DEFINE_integer('valid_step', valid_step_, 'checkpoint number for validation')
     flags.DEFINE_integer('valid_step', 20, 'checkpoint number for validation')
-    flags.DEFINE_integer('valid_num_steps', 1449, '= number of validation samples')
-    flags.DEFINE_string('valid_data_list', './dataset/val.txt', 'validation data list filename')
+    flags.DEFINE_integer('valid_num_steps', os.enviorn['NR_VAL'], '= number of validation samples')
+    flags.DEFINE_string('valid_data_list', os.environ['VALDATALIST'], 'validation data list filename')
 
     # prediction / saving outputs for testing or validation
     flags.DEFINE_string('out_dir', 'output', 'directory for saving outputs')
     flags.DEFINE_integer('test_step', 50000, 'checkpoint number for testing/validation')
     flags.DEFINE_integer('test_num_steps', 1449, '= number of testing/validation samples')
-    flags.DEFINE_string('test_data_list', './dataset/val.txt', 'testing/validation data list filename')
+    flags.DEFINE_string('test_data_list', os.environ['VALDATALIST'], 'testing/validation data list filename')
     flags.DEFINE_boolean('visual', True, 'whether to save predictions for visualization')
 
     # data
-    flags.DEFINE_string('data_dir', '../VOC2012', 'data directory')
-    flags.DEFINE_integer('batch_size', 10, 'training batch size')
-    flags.DEFINE_integer('input_height', 321, 'input image height')
-    flags.DEFINE_integer('input_width', 321, 'input image width')
-    flags.DEFINE_integer('num_classes', 21, 'number of classes')
+    flags.DEFINE_string('data_dir', os.environ['DATASET'], 'data directory')
+    flags.DEFINE_integer('batch_size', os.environ['BATCH_SIZE'], 'training batch size')
+    flags.DEFINE_integer('input_height', os.environ['IMG_SIZE'], 'input image height')
+    flags.DEFINE_integer('input_width', os.environ['IMG_SIZE'], 'input image width')
+    flags.DEFINE_integer('num_classes', os.environ['NUM_CLASSES'], 'number of classes')
     flags.DEFINE_integer('ignore_label', 255, 'label pixel value that should be ignored')
     flags.DEFINE_boolean('random_scale', True, 'whether to perform random scaling data-augmentation')
     flags.DEFINE_boolean('random_mirror', True, 'whether to perform random left-right flipping data-augmentation')
@@ -96,7 +85,6 @@ def main(_):
             FLAGS = configure()
             FLAGS.__flags['num_steps'] = num_steps
             FLAGS.__flags['max_steps'] = num_steps*num_iterations
-#            sess = tf.Session()
 
             for i in range(0, num_iterations):
                 write_log ('Iteration: %d' % (i+1), FLAGS.__flags['logfile'])
--- setup.sh
+++ setup.sh
@@ -1,3 +1,30 @@
 #!/bin/sh
+
+PROJECT_DIR="$HOME/DL/Deeplab-v2--ResNet-101--Tensorflow"
 module purge
 module load StdEnv gcc/4.8.5 python_gpu/3.6.1
+if [[ $1 == cityscapes ]]; then
+	echo "Use cityscapes dataset"
+	export DATASET="/cluster/scratch/$USER/cityscapes"
+	export DATALIST="$PROJECT_DIR/dataset_cityscapes/train_fine.txt"
+	export VALDATALIST="$PROJECT_DIR/dataset_cityscapes/val_fine.txt"
+	export NR_VAL=500
+	export BATCH_SIZE=3
+	export IMG_SIZE=719
+	export NUM_CLASSES=19
+
+else
+	echo "Use VOC2012 dataset"
+	export DATASET="$HOME/DL/VOC2012"
+	export DATALIST="$PROJECT_DIR/dataset/train.txt"
+	export VALDATALIST="$PROJECT_DIR/dataset/val.txt"
+	export NR_VAL=1449
+	export BATCH_SIZE=10
+	export IMG_SIZE=321
+	export NUM_CLASSES=21
+fi
+
+
+
+
+
