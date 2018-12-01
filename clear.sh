24a32e72c95f240dd7170f7dd89e077d74ec634d
Thomas Ziegler <zieglert@ethz.ch>
2018-12-01 11:32:02 +0100

    use parameter.json file in scripts

--- clear.sh
+++ clear.sh
@@ -2,5 +2,5 @@
 
 DATE=`date +%Y%m%d-%H%M%S`
 FILE_NAME="log_$DATE.tar.gz"
-tar -cvzf "$FILE_NAME" parameters log.txt log/* model/*
+tar -cvzf "$FILE_NAME" parameters.json log.txt log/* model/*
 mv $FILE_NAME ~/ 
