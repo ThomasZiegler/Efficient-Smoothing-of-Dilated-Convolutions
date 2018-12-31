Fix Gaussian such that sigma has a gradient

dilated.py
@@ -76,40 +76,49 @@ def _averaged_dilated_conv2d(x, kernel_size, num_o, dilation_factor, name, top_s
         return o
 
 
+
+
+
+
+
 def _gaussian_dilated_conv2d(x, kernel_size, num_o, dilation_factor, name, top_scope, biased=False):
     """
     Dilated conv2d with antecedent gaussian filter and without BN or relu.
     """
     num_x = x.shape[3].value
-
     filter_size = dilation_factor - 1
 
-        #o = tf.expand_dims(x, -1)
-    #o = tf.nn.conv3d(o, w_gauss, strides=[1,1,1,1,1], padding='SAME')
-    #o = tf.squeeze(o, -1)
 
     with tf.variable_scope(name) as scope:
         # perform gaussian filtering (as seprable convolution)
         # init sigma value with 1
-        sigma_init = 1.75
+        sigma_init = 1.00
         init = tf.constant_initializer(sigma_init)
         sigma = tf.get_variable('gauss_sigma', shape=[1], initializer=init)
+
         # create kernel grid
-        ax = tf.range(-filter_size//2+1, filter_size//2+1, dtype=tf.float32)
-        xx, yy = tf.meshgrid(ax, ax)
-        # calculate weight and reshape to correct shape
-        w_gauss_value = tf.exp(-(xx**2 + yy**2) / (2.*sigma**2))
-        w_gauss_value = w_gauss_value / tf.reduce_sum(w_gauss_value)
+        ax = np.arange(-filter_size // 2 + 1., filter_size // 2 + 1.)
+        xx, yy = np.meshgrid(ax, ax)
+        kernel = np.exp(-(xx**2 + yy**2)) 
 
-        # dublicate kernel num_x times
-        w_gauss_value = tf.tile(tf.expand_dims(w_gauss_value,-1), [1,1,num_x])
-        # add expand one dimension to match depthwise_conv2d_native filter requirement
-        w_gauss_value = tf.expand_dims(w_gauss_value,-1)
-        w_gauss = tf.Variable(w_gauss_value, name='w_gauss')
 
-        
-        o = tf.nn.depthwise_conv2d_native(x, w_gauss, [1,1,1,1], padding='SAME')
+        mask = np.zeros([filter_size,filter_size, 1, 1, 1], dtype=np.float32)
+        mask[:, :, 0, 0, 0] = kernel
 
+        w_gauss_value = tf.Variable(tf.constant(0.0,
+                                    shape=[filter_size,filter_size, 1,1,1]), name='w_gauss_value',trainable=False)
+
+        # create gaussian filter 
+        w_gauss_value = tf.add(w_gauss_value, tf.constant(mask, dtype=tf.float32))
+        w_gauss_value = tf.div(w_gauss_value, tf.exp(2.0 * sigma**2))
+        w_gauss_value = tf.div(w_gauss_value, tf.reduce_sum(w_gauss_value))
+
+        # perform separable convolution
+        o_gauss = tf.expand_dims(x, -1)
+        o_gauss = tf.nn.conv3d(o_gauss, w_gauss_value, strides=[1,1,1,1,1], padding='SAME')
+        o_gauss = tf.squeeze(o_gauss, -1)
+
+        # perform dilated convolution 
         w = tf.get_variable('weights', shape=[kernel_size, kernel_size, num_x, num_o])
         o = tf.nn.atrous_conv2d(o, w, dilation_factor, padding='SAME')
         if biased:
@@ -178,8 +187,6 @@ def _combinational_layer(x, kernel_size, num_o, dilation_factor, name, top_scope
             b = tf.get_variable('biases', shape=[num_o])
             o = tf.nn.bias_add(o, b)
         return o
-
-   
 
 
 def _get_c_vector(name):